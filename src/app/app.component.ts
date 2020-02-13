3df43f23d50b43e74deec19fdf53ad179edb6164
boyang <yang.wilby@gmail.com>
2020-02-13 21:05:32 +0100

    1. pwa, 2. material, 3. login/logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, EventEmitter, Output} from '@angular/core';
+import {MatSidenav} from '@angular/material/sidenav';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {User} from './_models';
+import {AuthenticationService} from './_services';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'untitled2';
+
+  title = 'untitled1';
+  currentUser: User;
+  mobileQuery: MediaQueryList;
+  nav = [
+    {
+      title: 'Home',
+      path: '/home'
+    },
+    {
+      title: 'My Account',
+      path: '/auth'
+    }
+  ];
+  private mobileQueryListener: () => void;
+  @Output() toggleSideNav = new EventEmitter();
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private router: Router,
+              private authenticationService: AuthenticationService) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this.mobileQueryListener);
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
+
+  toggleMobileNav(nav: MatSidenav) {
+    if (this.mobileQuery.matches) {
+      nav.toggle();
+    }
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
+import {MatSliderModule} from '@angular/material/slider';
+import {HomeComponent} from './components/home/home.component';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {RouterModule} from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {appRoutingModule} from './app.routing';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import {ErrorInterceptor, fakeBackendProvider, JwtInterceptor} from './_helpers';
+import {LoginComponent} from './login';
+import {RegisterComponent} from './register';
+import {AlertComponent} from './_components';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    AlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
+    MatSliderModule,
+    MatListModule,
+    MatSidenavModule,
+    RouterModule,
+    MatToolbarModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    appRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
